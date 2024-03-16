373729769730179cb657122782d2fb995cca81e6
phuc <nguyendaihoangphuc24@gmail.com>
2024-03-16 16:57:47 +0700

    TestFail

--- test_sum.py
+++ test_sum.py
@@ -4,7 +4,7 @@ from sum import sum
 class TestSum(unittest.TestCase):
 
     def test_sum_positive_numbers(self):
-        self.assertEqual(sum(1, 2), 3)  # Sửa giá trị kỳ vọng để kiểm tra lỗi
+        self.assertEqual(sum(1, 2), 4)  # Sửa giá trị kỳ vọng để kiểm tra lỗi
 
     def test_sum_negative_numbers(self):
         self.assertEqual(sum(-1, -2), -3, "Should be -3")
